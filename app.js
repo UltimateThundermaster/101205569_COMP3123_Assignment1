697941afaa8a88bf8634c3b2e9c61a6c7dd07aa4
Artem Elagin <artem.elagin@georgebrown.ca>
2022-10-23 23:56:30 -0400

    adding screenshots

--- app.js
+++ app.js
@@ -5,14 +5,15 @@ const employeeRouter = require("./routes/employee")
 const auth = require("./middleware/auth")
 const app = express()
 
-const mongoAtlasUri = ""
+const mongoAtlasUri = "mongodb+srv://ThunderMaster:Pass123@cluster0.phfivi1.mongodb.net/Assignment1?retryWrites=true&w=majority"
 
 app.use(express.json())
+app.use(express.urlencoded())
 
 app.use('/api/user', userRouter)
 app.use('/api/emp', auth, employeeRouter)
 
-const port = 8000
+const port = 8150
 
 app.listen(port, () => {
     try {
